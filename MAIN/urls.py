ce1e8f56bcfa6c774b76f26669751fe83dba0718
prosper <edwardprosper001@gmail.com>
2022-10-17 22:16:27 +0100

    updates

--- MAIN/urls.py
+++ MAIN/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from core.views import HelloView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', HelloView.as_view(), name='hello'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     
